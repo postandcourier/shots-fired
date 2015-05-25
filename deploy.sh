36d26ed0888b0f9defe2ca12d91799ca50972a38
J Emory Parker <eparker@postandcourier.com>
2015-05-25 19:33:04 -0400

    adding pages from db

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "Building Meteor..."
+meteor build --directory /var/www/shots-fired-prod/
+pushd /var/www/shots-fired-prod/bundle/programs/server/
+echo "Installing necessary node modules..."
+npm install
+echo "Restarting service..."
+service shotsfired restart
